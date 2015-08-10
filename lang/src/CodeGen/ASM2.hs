eee699cfb8895d3a280ae5a6669e0b27f33f7d81
piotrMocz <piotr.moczurad@gmail.com>
2015-08-10 23:49:05 +0200

    Beginnings of assembly generation

---
+++ lang/src/CodeGen/ASM2.hs
@@ -0,0 +1,96 @@
+module CodeGen.ASM2 where
+
+import Control.Lens
+import Control.Monad (sequence)
+import Data.Functor  ((<$>))
+import Data.List     (intercalate)
+
+
+data Addr = SAddr Int | VAddr Int deriving (Show, Eq, Ord)
+
+newtype SVal  = SVal  Int    deriving (Eq, Ord)
+newtype VVal  = VVal  [SVal] deriving (Eq, Ord)
+
+
+sMinAddr = 0
+sMaxAddr = 32
+vMinAddr = 0
+vMaxAddr = 32
+
+
+sMinVal = -128
+sMaxVal = 127
+
+
+createSAddr :: Int -> Maybe Addr
+createSAddr n = if n >= sMinAddr && n < sMaxAddr then Just $ SAddr n
+                                                 else Nothing
+
+
+createVAddr :: Int -> Maybe Addr
+createVAddr n = if n >= vMinAddr && n < vMaxAddr then Just $ VAddr n
+                                                 else Nothing
+
+
+createSVal :: Int -> Maybe SVal
+createSVal v = if v >= sMinVal && v < sMaxVal then Just $ SVal v
+                                              else Nothing
+
+
+createVVal :: [Int] -> Maybe VVal
+createVVal vs = VVal <$> (sequence $ map createSVal vs)
+
+
+data ASMInstruction = LoadS  { addr    :: Addr  }
+                    | LoadV  { vaddr   :: Addr  }
+                    | StoreS { saddr   :: Addr  }
+                    | StoreV { vaddr   :: Addr  }
+                    | PushS  { sval    :: SVal  }
+                    | PushV  { vval    :: VVal  }
+                    | AddS
+                    | AddV
+                    | SubS
+                    | SubV
+                    | MulS
+                    | MulV
+                    | DivS
+                    | DivV deriving (Eq, Ord)
+
+
+type ASMCode = [ASMInstruction]
+
+
+class MakeASM a where
+    makeASM :: a -> String
+
+
+instance MakeASM Addr where
+    makeASM (SAddr a) = show a
+    makeASM (VAddr a) = show a
+
+instance MakeASM SVal where
+    makeASM (SVal a)     = show a
+
+instance MakeASM VVal where
+    makeASM (VVal svals) = "[" ++ (map makeASM svals & intercalate ",") ++ "]"
+
+
+instance MakeASM ASMInstruction where
+    makeASM (LoadS  addr) = "LDS "   ++ makeASM addr
+    makeASM (LoadV  addr) = "LDV "   ++ makeASM addr
+    makeASM (StoreS addr) = "STS "   ++ makeASM addr
+    makeASM (StoreV addr) = "STV "   ++ makeASM addr
+    makeASM (PushS  val ) = "PUSHS " ++ makeASM val
+    makeASM (PushV  val ) = "PUSHV " ++ makeASM val
+    makeASM  AddS         = "ADDS"
+    makeASM  AddV         = "ADDV"
+    makeASM  SubS         = "SUBS"
+    makeASM  SubV         = "SUBV"
+    makeASM  MulS         = "MULS"
+    makeASM  MulV         = "MULV"
+    makeASM  DivS         = "DIVS"
+    makeASM  DivV         = "DIVV"
+
+
+instance Show ASMInstruction where
+    show instr = makeASM instr
--- lang/src/CodeGen/Generator.hs
+++ lang/src/CodeGen/Generator.hs
@@ -2,39 +2,138 @@
 
 module CodeGen.Generator where
 
-import Control.Monad.State
-import Control.Lens
-import Data.Monoid
+import           Control.Lens
+import           Control.Monad.Trans.Except
+import           Control.Monad.State
+import           Control.Monad              (sequence, mapM)
+import qualified Data.Map                   as Map
+import           Data.Map                   (Map)
+import           Data.Monoid
 
-import CodeGen.ASM as ASM
-import Parser.AST  as AST
+import           CodeGen.ASM2               as ASM
+import qualified Parser.AST                 as AST
+
+
+
+type SymTable = Map String VarInfo
+
+
+data VarType = Scalar | Vector deriving (Show, Eq, Ord)
+
+
+data VarInfo = VarInfo {
+               _varName :: String
+             , _varType :: VarType
+             , _varAddr :: Addr
+             } deriving (Show, Eq, Ord)
+
+makeLenses ''VarInfo
 
 
 data GeneratorData = GeneratorData {
-                     _asmCode    :: ASM.ASMCode
-                   , _moduleAST  :: AST.Module
+                     _asmCode  :: ASM.ASMCode
+                   , _modAST   :: AST.Module
+                   , _symTable :: SymTable
+                   , _avAddrsS :: [Addr]
+                   , _avAddrsV :: [Addr]
                    } deriving (Show, Eq, Ord)
 
 makeLenses ''GeneratorData
 
-type GeneratorState a = State GeneratorData a
 
+newtype GeneratorError = GeneratorError { _err :: String } deriving (Show, Eq, Ord)
+makeLenses ''GeneratorError
+
+
+type GeneratorState a  = ExceptT GeneratorError (State GeneratorData) a
+
+
+--------------------------------------------------------------
+-- Processing AST
+--------------------------------------------------------------
+
+processAST :: AST.Module -> GeneratorState () -- ASM.ASMCode
+processAST ast = mapM_ processExpr ast
+
+
+processExpr :: AST.Expr -> GeneratorState ()
+processExpr expr = case expr of
+    AST.Lit i           -> pushS (fromIntegral i      :: Int  ) -- TODO change integers to ints
+    AST.VecLit is       -> pushV (map fromIntegral is :: [Int])
+    AST.VarE var        -> lookupVar var
+    AST.Assign var expr -> createVar var expr
+    _                   -> throwE $ GeneratorError "Instruction not yet implemented"
+
+
+createVar :: AST.Var -> AST.Expr -> GeneratorState ()
+createVar var expr = do
+    addr <- getAvailableAddrS
+    processExpr expr
+    pushVarInfo $ VarInfo var Scalar addr
+    pushASMInstr $ ASM.StoreS addr
+
+
+--------------------------------------------------------------
+-- Utility state functions
+--------------------------------------------------------------
 
 pushASMInstr :: ASM.ASMInstruction -> GeneratorState ()
 pushASMInstr instr = do
-  genData <- get
-  let newData = genData & asmCode %~ (instr :)
-  put newData
+    genData <- get
+    let newData = genData & asmCode %~ (instr :)
+    put newData
 
 
-popExpr :: GeneratorState (Maybe AST.Expr)
-popExpr = do
-  genData <- get
-  if null $ genData ^. moduleAST
-    then return Nothing
-    else do
-      let (e:es)  = genData ^. moduleAST
-          newData = genData &  moduleAST .~ es
-      put newData
-      return $ Just e    
+getAvailableAddrS :: GeneratorState Addr
+getAvailableAddrS = do
+    st <- get
+    let addr = st ^. avAddrsS
+    case addr of
+        []     -> throwE $ GeneratorError "No more address space for new variables"
+        (a:as) -> do
+            let newSt = st & avAddrsS .~ as
+            put newSt
+            return a
 
+
+getAvailableAddrV :: GeneratorState Addr
+getAvailableAddrV = do
+    st <- get
+    let addr = st ^. avAddrsV
+    case addr of
+        []     -> throwE $ GeneratorError "No more address space for new variables"
+        (a:as) -> do
+            let newSt = st & avAddrsV .~ as
+            put newSt
+            return a
+
+
+pushVarInfo :: VarInfo -> GeneratorState ()
+pushVarInfo vInfo = do
+    st <- get
+    let newSt = st & symTable %~ (Map.insert (vInfo ^. varName) vInfo)
+    put st
+
+
+lookupVar :: AST.Var -> GeneratorState ()
+lookupVar var = do
+    st <- get
+    case Map.lookup var (st ^. symTable) of
+        Nothing    -> throwE . GeneratorError $ "Using undeclared variable name: " ++ var
+        Just vInfo -> pushASMInstr $ ASM.LoadS (vInfo ^. varAddr) -- TODO this function can be
+                                                                  -- generic with respect to var type
+
+
+pushS :: Int -> GeneratorState ()
+pushS v = case ASM.createSVal v of
+    Nothing  -> throwE $ GeneratorError "Scalar value out of range"
+    Just val -> pushASMInstr $ PushS val
+
+
+pushV :: [Int] -> GeneratorState ()
+pushV vs = case ASM.createVVal vs of
+    Nothing   -> throwE $ GeneratorError "Vector value out of range"
+    Just vals -> pushASMInstr $ PushV vals
+
+
+emptyState = GeneratorData [] [] Map.empty
