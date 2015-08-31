e6d9820788a45e2a22b086ccaeebb28605e6f191
piotrMocz <piotr.moczurad@gmail.com>
2015-08-31 08:55:43 +0200

    Loop-something added

--- lang/src/Parser/AST.hs
+++ lang/src/Parser/AST.hs
@@ -2,22 +2,35 @@ module Parser.AST where
 
 import Data.Default
 
+
 type Module = [Expr]
+
 
 data Expr = Lit Integer
           | VecLit [Integer]
-          | VarE Var
+          | VarE VarName
           | BinOp Op Expr Expr
           | If Expr [Expr] [Expr]
-          | Assign Var Expr
-          | Decl Var Type Expr
+          | Assign VarName Expr
+          | Decl VarName Type Expr
           deriving (Show, Eq, Ord)
 
-type Var = String
-data Type = Scalar | Vector Integer deriving (Show, Eq, Ord)
+
+type VarName = String
+data Type    = Scalar | Vector Integer deriving (Show, Eq, Ord)
 
 
 data Op = Add Type | Sub Type | Mul Type | Div Type deriving (Show, Eq, Ord)
 
+
 instance Default Type where
     def = Scalar
+
+
+------- UTILS --------
+loopIf :: Int -> Int -> (Expr, Expr, Expr)
+loopIf lab reps =
+    ( Decl varName Scalar (Lit . fromIntegral $ reps + 1)        -- counter
+    , Assign varName (BinOp (Sub Scalar) (VarE varName) (Lit 1)) -- decrement counter
+    , VarE varName                                               -- evaluate counter
+    ) where varName = "counter" ++ show lab
