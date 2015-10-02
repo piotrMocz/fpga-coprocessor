78ec94274ee2b63867f7091b240b23c65ec54156
piotrMocz <piotr.moczurad@gmail.com>
2015-10-03 01:11:43 +0200

    A working implementation of UART from Bealto

--- fpga/fpga-coprocessor/fpga_coprocessor.vhd
+++ fpga/fpga-coprocessor/fpga_coprocessor.vhd
@@ -1,5 +1,9 @@
+-- based on code from bealto.com
+
 library IEEE;
-use IEEE.std_logic_1164.all;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+
 
 entity fpga_coprocessor is
   port (
@@ -42,12 +46,125 @@ entity fpga_coprocessor is
 	GPIO       : inout std_logic_vector(33 downto 0);
 	GPIO_IN    : in    std_logic_vector(1  downto 0)
 	);
+	
 end entity fpga_coprocessor;
 
+
 architecture arch of fpga_coprocessor is
+
+    
+component basic_uart is
+generic (
+  DIVISOR: natural
+);
+port (
+  clk: in std_logic;   -- system clock
+  reset: in std_logic;
+  
+  -- Client interface
+  rx_data: out std_logic_vector(7 downto 0);  -- received byte
+  rx_enable: out std_logic;  -- validates received byte (1 system clock spike)
+  tx_data: in std_logic_vector(7 downto 0);  -- byte to send
+  tx_enable: in std_logic;  -- validates byte to send if tx_ready is '1'
+  tx_ready: out std_logic;  -- if '1', we can send a new byte, otherwise we won't take it
+  
+  -- Physical interface
+  rx: in std_logic;
+  tx: out std_logic
+);
+end component;
+
+type fsm_state_t is (idle, received, emitting);
+type state_t is
+record
+  fsm_state: fsm_state_t; -- FSM state
+  tx_data: std_logic_vector(7 downto 0);
+  tx_enable: std_logic;
+end record;
+
+signal reset: std_logic;
+signal reset_btn : std_logic;
+signal uart_rx_data: std_logic_vector(7 downto 0);
+signal uart_rx_enable: std_logic;
+signal uart_tx_data: std_logic_vector(7 downto 0);
+signal uart_tx_enable: std_logic;
+signal uart_tx_ready: std_logic;
+
+signal state,state_next: state_t;
+
 begin
 
-	LED(0) <= KEY(0); 
-	LED(1) <= KEY(1);
+  reset_btn <= KEY(0);
 
-end architecture arch;
+  basic_uart_inst: basic_uart
+  generic map (DIVISOR => 326)
+  port map (
+    clk => CLOCK_50, reset => reset,
+    rx_data => uart_rx_data, rx_enable => uart_rx_enable,
+    tx_data => uart_tx_data, tx_enable => uart_tx_enable, tx_ready => uart_tx_ready,
+    rx => GPIO(11),
+    tx => GPIO( 9)
+  );
+
+  reset_control: process (reset_btn) is
+  begin
+    if reset_btn = '1' then
+      reset <= '0';
+    else
+      reset <= '1';
+    end if;
+  end process;
+  
+  -- pmod_1 <= uart_tx_enable;
+  -- pmod_2 <= uart_tx_ready;
+  
+  fsm_clk: process (CLOCK_50,reset) is
+  begin
+    if reset = '1' then
+      state.fsm_state <= idle;
+      state.tx_data <= (others => '0');
+      state.tx_enable <= '0';
+    else
+      if rising_edge(CLOCK_50) then
+        state <= state_next;
+      end if;
+    end if;
+  end process;
+
+  fsm_next: process (state,uart_rx_enable,uart_rx_data,uart_tx_ready) is
+  begin
+    state_next <= state;
+    case state.fsm_state is
+    
+    when idle =>
+      if uart_rx_enable = '1' then
+        state_next.tx_data <= uart_rx_data;
+        state_next.tx_enable <= '0';
+        state_next.fsm_state <= received;
+      end if;
+      
+    when received =>
+      if uart_tx_ready = '1' then
+        state_next.tx_enable <= '1';
+        state_next.fsm_state <= emitting;
+      end if;
+      
+    when emitting =>
+      if uart_tx_ready = '0' then
+        state_next.tx_enable <= '0';
+        state_next.fsm_state <= idle;
+      end if;
+      
+    end case;
+  end process;
+  
+  fsm_output: process (state) is
+  begin
+  
+    uart_tx_enable <= state.tx_enable;
+    uart_tx_data <= state.tx_data;
+    led <= state.tx_data;
+    
+  end process;
+
+end arch;
